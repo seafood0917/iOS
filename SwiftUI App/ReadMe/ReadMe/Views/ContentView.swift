6b1ba1de3e73721bbf679794db6a3f4a0b6d20fd
seafood0917 <seafood0917@gmail.com>
2020-12-01 16:20:14 +0900

    Added Sections

--- SwiftUI App/ReadMe/ReadMe/Views/ContentView.swift
+++ SwiftUI App/ReadMe/ReadMe/Views/ContentView.swift
@@ -35,8 +35,7 @@ struct ContentView: View {
                     NewBookView.init
                 )
 
-                ForEach(library.sortedBooks) { book in
-                    BookRow(book: book)
+              ForEach(Section.allCases, id: \.self) { SectionView(section: $0)
                 }
             }
             .navigationBarTitle("My Library")
@@ -44,7 +43,7 @@ struct ContentView: View {
     }
 }
 
-struct BookRow: View { //
+private struct BookRow: View { //
     @ObservedObject var book: Book
     @EnvironmentObject var library: Library
 
@@ -84,6 +83,40 @@ struct BookRow: View { //
             .padding(.vertical, 8)
         }
     }
+}
+
+private struct SectionView: View {
+  let section: Section
+  @EnvironmentObject var library: Library
+  var title: String {
+    switch section {
+    case .readMe:
+      return "Read Me!"
+    case .finished:
+      return "Finished!"
+    }
+  }
+  
+  var body: some View {
+    if let books = library.manuallySortedBooks[section] {
+      SwiftUI.Section(
+        header:
+          ZStack {
+            Image("BookTexture")
+            .resizable()
+            .scaledToFit()
+            Text(title)
+              .font(.custom("American Typewriter", size: 24))
+          }
+          .listRowInsets(.init())
+      )
+      {
+        ForEach(books) {
+          BookRow(book: $0)
+        }
+      }
+    }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
